a7afeec33c47ce96d3fd38802fb7e7450abee9aa
Krolaf <jerome@grafitoid.com>
2025-05-20 11:01:02 +0400

    remove_sql_file

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO game_mode (name, description, min_players, max_players, score_to_win) VALUES
-  ('Solo', 'Un joueur contre un autre', 1, 2, 10),
-  ('Duo', 'Deux équipes de deux joueurs', 2, 2, 20),
-  ('Squad', 'Équipe de 4 joueurs', 4, 4, 50); 
