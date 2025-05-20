1af6e73bb9e56340a0c3bf963d6b6d6ac3ebfe3c
Krolaf <jerome@grafitoid.com>
2025-05-20 08:43:50 +0400

    first_commit

---
+++ src/main/java/com/java/tournois/TournoisApplication.java
@@ -0,0 +1,13 @@
+package com.java.tournois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/tournois/TournoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.tournois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
